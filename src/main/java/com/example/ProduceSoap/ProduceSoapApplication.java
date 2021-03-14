5432336b8e6192f62ff5f6d20420ac063e6f98d0
nueltogo <nunana.togo@turntabl.io>
2021-03-14 10:56:51 +0000

    start of project

---
+++ src/main/java/com/example/ProduceSoap/ProduceSoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProduceSoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduceSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduceSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProduceSoap/ProduceSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProduceSoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduceSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
